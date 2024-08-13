27babedd9086027f4179f8a3cd0ac5aacce46c01
ab.zare <ab.zare@git.mofid.dev>
2024-08-13 10:59:39 +0330

    composite

---
+++ Composite.kt
@@ -0,0 +1,54 @@
+interface FileSystemEntity {
+    fun getName(): String
+    fun getChildren(): MutableList<FileSystemEntity>? = null
+    fun show(): String
+}
+
+class File(private val name: String) : FileSystemEntity {
+    override fun getName() = name
+    override fun getChildren() = null
+    override fun show() = "$name::f"
+}
+
+class Directory(private val name: String) : FileSystemEntity {
+    override fun getName() = name
+    private val subSet = mutableListOf<FileSystemEntity>()
+    override fun getChildren() = subSet
+    override fun show() = "$name::dir( ${getChildren().map { it.getName() }} )"
+
+    fun add(fileSystemEntity: FileSystemEntity): Directory {
+        subSet.add(fileSystemEntity)
+        return this
+    }
+}
+
+fun main() {
+
+    val homeDir = Directory("home")
+        .add(
+            Directory("movies")
+                .add(File("Tetris.mkv"))
+                .add(File("TakeShelter.mp4"))
+                .add(File("Stalker.mp4"))
+                .add(Directory("independent-cinema"))
+        )
+        .add(
+            Directory("books")
+                .add(File("Learn Rust Programming.pdf"))
+                .add(File("The.Pragmatic.Programmer.pdf"))
+                .add(File("Functional-Kotlin.pdf"))
+                .add(File("Java-Concurrency-in-Practice.pdf"))
+                .add(Directory("philosophy"))
+        )
+        .add(
+            Directory("Downloads")
+        )
+        .add(
+            File("error.log")
+        )
+
+    println(homeDir.show())
+    println(homeDir.getChildren().last().show())
+    println(homeDir.getChildren().first().show())
+    println(homeDir.getChildren().first().getChildren()!!.last().show())
+}
